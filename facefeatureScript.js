195fbdcfa47f6d1d8868f363d3f4c1d775a45b0f
wuitesting <wuitesting@gmail.com>
2024-12-02 13:34:03 +0530

    Update facefeatureScript.js

--- facefeatureScript.js
+++ facefeatureScript.js
@@ -5,8 +5,8 @@ Promise.all([
 // async function loadModels() {
   //    console.log("Loading models...");
   //faceapi.nets.tinyFaceDetector.loadFromUri('model'),loadFaceLandmarkTinyModel(
-  faceapi.loadFaceLandmarkTinyModel('model'),
-  faceapi.loadFaceLandmarkModel('model')
+  faceapi.loadFaceLandmarkTinyModel('/model'),
+  faceapi.loadFaceLandmarkModel('/model')
  // faceapi.nets.faceLandmark68Net.loadFromUri('model')
  //
  //   }
