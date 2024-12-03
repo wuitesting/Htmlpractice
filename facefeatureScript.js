e399070db3e6d09c1d38713110537b033c780084
wuitesting <wuitesting@gmail.com>
2024-12-03 17:04:44 +0530

    Update facefeatureScript.js

--- facefeatureScript.js
+++ facefeatureScript.js
@@ -42,11 +42,11 @@ async function startMeasurement() {
   //console.log(`Face Length: ${faceLength.toFixed(2)} pixels`);
   outp.innerHTML = `
   Eye width: ${eyeWidth.toFixed(2)} cm
-  //forehead Height: ${foreheadWidth.toFixed(2)} cm
-  //face Height: ${faceLength.toFixed(2)} cm
+ 
   jaw Width: ${jawWidth.toFixed(2)} cm
 `;
-
+ //forehead Height: ${foreheadWidth.toFixed(2)} cm
+  //face Height: ${faceLength.toFixed(2)} cm
 }
 
 function distanceBetweenPoints(point1, point2) {
